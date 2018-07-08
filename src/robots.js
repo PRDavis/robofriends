ee136a37fec00999fd2f72c92a57760a69563839
Paul Davis <paulandcindy@gmail.com>
2018-07-08 13:53:42 -0400

    add robots.js for objects

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Opie Taylor',
+        username: 'Ronnie',
+        email: 'rhoward@hollywood.net'
+    },
+    {
+        id: 3,
+        name: 'Harry Bosch',
+        username: 'Detective',
+        email: 'harry@lapd.gov'
+    }
+]
